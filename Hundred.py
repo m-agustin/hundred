9428a51850ebf11ea681d6477dd0559015fc7210
Marichu Agustin <marichuanne@Marichus-Laptop.local>
2021-03-08 10:49:00 -0500

    change

---
+++ Hundred.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+#Asks user to input their name & age
+name = input("What is your name: ")
+age = int(input("What is you age: "))
+
+#Checks the current year we are on and subtracts it with the user's age and adds 100
+#Determines what year the user will turn 100
+hundred = str((datetime.now().year - age) + 100)
+
+#Informs the user's results
+print(name + " you will turn 100 in year " + hundred)
